8e3848375627e0e6e444e9a1c38731c5e63d3220
zeynab335 <zezemahmoud@domain.com>
2022-09-04 19:16:24 +0200

    finishing Header in Facebook-clone

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,32 @@
 import React from "react";
 import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import FlagIcon from "@material-ui/icons/Flag";
+import AddIcon from "@material-ui/icons/Add";
+import ForumIcon from "@material-ui/icons/Forum";
+import StorefrontIcon from "@material-ui/icons/Storefront";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import SupervisedUserCircleIcon from "@material-ui/icons/SupervisedUserCircle";
+import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
 
 // css file
 import "./Header.css";
+import { Avatar, IconButton } from "@material-ui/core";
 
 const Header = () => {
+  const HeaderIcons = [
+    <HomeIcon />,
+    <FlagIcon />,
+    <StorefrontIcon />,
+    <SubscriptionsIcon />,
+    <SupervisedUserCircleIcon />,
+  ];
+  const HeaderBtnIcons = [
+    <AddIcon />,
+    <ForumIcon />,
+    <NotificationsActiveIcon />,
+  ];
+
   return (
     <div className="header">
       {/* Header Left */}
@@ -14,15 +36,34 @@ const Header = () => {
           alt=""
         />
         <div className="header__search">
-          <SearchIcon />
+          <SearchIcon className="header__search__icon" />
+          <input type="text" placeholder="Search For..." />
         </div>
       </div>
 
       {/* Header Center */}
-      <div className="header__middle"></div>
+      <div className="header__middle">
+        {HeaderIcons.map((icon, index) => (
+          <div
+            className={`header__option ${
+              index === 0 ? "header__option--active" : ""
+            }`}
+          >
+            {icon}
+          </div>
+        ))}
+      </div>
 
       {/* Header right */}
-      <div className="header__right"></div>
+      <div className="header__right">
+        <div className="header__info">
+          <Avatar />
+          <h4> ZEZE </h4>
+        </div>
+        {HeaderBtnIcons.map((icon) => (
+          <IconButton> {icon} </IconButton>
+        ))}
+      </div>
     </div>
   );
 };
